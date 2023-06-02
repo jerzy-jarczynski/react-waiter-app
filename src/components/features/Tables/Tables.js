c795762de429a3857d67d67c43b9207492bb6636
Jerzy Jarczynski <jerzy.jarczynski.95@gmail.com>
2023-06-02 22:23:59 +0200

    Add removeBookRequest

--- src/components/features/Tables/Tables.js
+++ src/components/features/Tables/Tables.js
@@ -1,28 +1,45 @@
 // bootstrap
 import { Row, Button, Col } from "react-bootstrap";
 // redux
-import { useSelector } from "react-redux";
-import { getAllTables } from "../../../redux/tablesRedux";
+import { useSelector, useDispatch } from "react-redux";
+import { getAllTables, removeTableRequest } from "../../../redux/tablesRedux";
+
+// router
+import { NavLink } from "react-router-dom";
 
 const Tables = ({ tables }) => {
 
+  const dispatch = useDispatch();
+
   const allTables = useSelector(getAllTables);
 
-  console.log(allTables);
+  const handleClick = (tableId) => {
+    dispatch(removeTableRequest(tableId));
+  };
+
+  if (allTables.length === 0) {
+    return (
+      <Row className="py-4">
+        <Col xs={12}>
+          Nothing to show
+        </Col>
+      </Row>      
+    );
+  }
 
   return (
     <Row className="my-3">
       {
-        allTables.map(table => (
-          <>
-            <Col xs={12} key={table.id} className="d-flex flex-row justify-content-start align-items-center pt-2 pb-4">
+        allTables.map(table => (      
+          <div key={table.id}>
+            <Col xs={12} className="d-flex flex-row justify-content-start align-items-center pt-2 pb-2">
               <h3 className="py-0 my-0 me-4">{ `Table ${table.id}` }</h3>
               <p className="py-0 my-0"><span className={ 'fw-bold' }>Status: </span>{ table.status }</p>
-              <Button variant="danger" className="ms-auto me-2">Remove</Button>
-              <Button variant="primary">Show more</Button>
+              <Button variant="danger" className="ms-auto me-2" onClick={() => handleClick(table.id)}>Remove</Button>
+              <Button variant="primary" as={NavLink} to={`/table/${table.id}`}>Show more</Button>
             </Col>
             <hr />
-          </>
+          </div>
         ))
       }
     </Row>
--- src/redux/tablesRedux.js
+++ src/redux/tablesRedux.js
@@ -4,9 +4,11 @@ export const getAllTables = state => state.tables;
 // actions
 const createActionName = actionName => `app/tables/${actionName}`;
 const UPDATE_TABLES = createActionName('UPDATE_TABLES');
+const REMOVE_TABLE = createActionName('REMOVE_TABLE');
 
 // action creators
 export const updateTables = payload => ({ type: UPDATE_TABLES, payload });
+export const removeTable = payload => ({ type: REMOVE_TABLE, payload });
 export const fetchTables = () => {
   return (dispatch) => {
     fetch('http://localhost:3131/api/tables')
@@ -14,10 +16,36 @@ export const fetchTables = () => {
       .then(tables => dispatch(updateTables(tables)));
   };
 };
+export const removeTableRequest = (tableId) => {
+  return (dispatch) => {
+    const options = {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    };
+
+    fetch(`http://localhost:3131/api/tables/${tableId}`, options)
+      .then(response => {
+        if (response.ok) {
+          console.log(`Table #${tableId} removed succesfully`);
+        } else {
+          console.log(`Failed to remove Table #${tableId}`);
+        }
+      })
+      .then(() => dispatch(removeTable(tableId)))
+      .then(() => dispatch(fetchTables()))
+      .catch(error => {
+        console.log(`Error occurred while removing Table #${tableId}`, error);
+      });
+  }
+};
 
 // reducer
 const tablesReducer = (statePart = [], action) => {
   switch (action.type) {
+    case REMOVE_TABLE:
+      return statePart.filter(table => table.id !== action.payload);
     case UPDATE_TABLES:
       return [ ...action.payload ];
     default:
