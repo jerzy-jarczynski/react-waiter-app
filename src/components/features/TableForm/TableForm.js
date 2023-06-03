e007ddff1e7d8d74c3850637603c70444fd20a77
Jerzy Jarczynski <jerzy.jarczynski.95@gmail.com>
2023-06-03 20:35:03 +0200

    Add styles for TablesForm

--- src/components/features/TableForm/TableForm.js
+++ src/components/features/TableForm/TableForm.js
@@ -3,26 +3,50 @@ import { Row, Col, Form, Button } from "react-bootstrap";
 const TableForm = () => (
   <Form>
     <Row>
-      <Col>
+      <Col xs={8}>
         <Form.Group>
-          <Form.Label>Status:</Form.Label>
-          <Form.Select>
-            <option>Busy</option>
-            <option>Free</option>
-            <option>Cleaning</option>
-            <option>Reserved</option>
-          </Form.Select>
+          <Row className="align-items-center">
+            <Col xs={2}>
+              <Form.Label className="mb-0">Status:</Form.Label>
+            </Col>
+            <Col>
+              <Form.Select>
+                <option>Busy</option>
+                <option>Free</option>
+                <option>Cleaning</option>
+                <option>Reserved</option>
+              </Form.Select>
+            </Col>
+          </Row>
         </Form.Group>
         <Form.Group>
-          <Form.Label>People</Form.Label>
-          <Form.Control type="number" />
-          {'/'}
-          <Form.Control type="number" />
+          <Row className="align-items-center justify-content-start">
+            <Col xs={2}>
+              <Form.Label className="mb-0">People:</Form.Label>
+            </Col>
+            <Col xs={2}>
+              <Form.Control type="number" />
+            </Col>
+            <Col xs={1} className="text-center">
+              {'/'}
+            </Col>
+            <Col xs={2}>
+              <Form.Control type="number" />
+            </Col>
+          </Row>
         </Form.Group>
         <Form.Group>
-            <Form.Label>Bill</Form.Label>
-            { '$' }
-            <Form.Control type="number" />
+          <Row className="align-items-center justify-content-start">
+            <Col xs={2}>
+              <Form.Label className="mb-0">Bill:</Form.Label>
+            </Col>
+            <Col xs={1}>
+              { '$' }
+            </Col>
+            <Col xs={2}>
+              <Form.Control type="number" />
+            </Col>
+          </Row>
         </Form.Group>
       </Col>
     </Row>
