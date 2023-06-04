d02100a3f1a043ed99110b4944e1881a6d33ffff
Jerzy Jarczynski <jerzy.jarczynski.95@gmail.com>
2023-06-04 19:49:58 +0200

    Add Bootstrap Spinner while waiting for fetch data from server

--- src/components/features/TableEditForm/TableEditForm.js
+++ src/components/features/TableEditForm/TableEditForm.js
@@ -1,30 +1,42 @@
-// components
 import TableForm from "../TableForm/TableForm";
-// redux
 import { useSelector, useDispatch } from "react-redux";
 import { getTableById, fetchTableDataRequest, changeTableRequest } from "../../../redux/tablesRedux";
-// Router
 import { useNavigate } from "react-router-dom";
-// react
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { Spinner } from "react-bootstrap";
 
 const TableEditForm = ({ tableId }) => {
-
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const [loading, setLoading] = useState(true); // Added loading state
 
   useEffect(() => {
     dispatch(fetchTableDataRequest(tableId));
   }, [dispatch, tableId]);
 
-  const tableData = useSelector(
-    (state) => getTableById(state, tableId)
-  );
+  const tableData = useSelector((state) => getTableById(state, tableId));
+
+  useEffect(() => {
+    if (tableData) {
+      setLoading(false); // Set loading state to false when data is available
+    }
+  }, [tableData]);
 
   const handleSubmit = (table) => {
     dispatch(changeTableRequest(table));
     navigate('/');
   };
+
+  if (loading) {
+    // Show spinner while loading
+    return (
+      <div className="d-flex justify-content-center">
+        <Spinner animation="border" role="status">
+          <span className="visually-hidden">Loading...</span>
+        </Spinner>
+      </div>
+    );
+  }
 
   if (!tableData) {
     return null;
--- src/components/features/Tables/Tables.js
+++ src/components/features/Tables/Tables.js
@@ -1,50 +1,107 @@
-// bootstrap
-import { Row, Button, Col } from "react-bootstrap";
-// redux
+import { Row, Button, Col, Modal, Spinner } from "react-bootstrap";
 import { useSelector, useDispatch } from "react-redux";
 import { getAllTables, removeTableRequest } from "../../../redux/tablesRedux";
-
-// router
 import { NavLink } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 const Tables = ({ tables }) => {
+  const [tableOutId, setTableOutId] = useState(0);
+  const [show, setShow] = useState(false);
+  const [loading, setLoading] = useState(true); // Added loading state
 
   const dispatch = useDispatch();
-
   const allTables = useSelector(getAllTables);
 
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   const handleClick = (tableId) => {
+    setTableOutId(tableId);
+    handleShow();
+  };
+
+  const handleRemove = (tableId) => {
+    handleClose();
     dispatch(removeTableRequest(tableId));
   };
+
+  useEffect(() => {
+    if (allTables.length > 0) {
+      setLoading(false);
+    }
+  }, [allTables]);
+
+  if (loading) {
+    // Show spinner while loading
+    return (
+      <Row className="my-3">
+        <Col className="d-flex justify-content-center">
+          <Spinner animation="border" role="status">
+            <span className="visually-hidden">Loading...</span>
+          </Spinner>
+        </Col>
+      </Row>
+    );
+  }
 
   if (allTables.length === 0) {
     return (
       <Row className="py-4">
-        <Col xs={12}>
-          Nothing to show
-        </Col>
-      </Row>      
+        <Col xs={12}>Nothing to show</Col>
+      </Row>
     );
   }
 
   return (
-    <Row className="my-3">
-      {
-        allTables.map(table => (      
+    <>
+      <Row className="my-3">
+        {allTables.map((table) => (
           <div key={table.id}>
-            <Col xs={12} className="d-flex flex-row justify-content-start align-items-center pt-2 pb-2">
-              <h3 className="py-0 my-0 me-4">{ `Table ${table.id}` }</h3>
-              <p className="py-0 my-0"><span className={ 'fw-bold' }>Status: </span>{ table.status }</p>
-              <Button variant="danger" className="ms-auto me-2" onClick={() => handleClick(table.id)}>Remove</Button>
-              <Button variant="primary" as={NavLink} to={`/table/${table.id}`}>Show more</Button>
+            <Col
+              xs={12}
+              className="d-flex flex-row justify-content-start align-items-center pt-2 pb-2"
+            >
+              <h3 className="py-0 my-0 me-4">{`Table ${table.id}`}</h3>
+              <p className="py-0 my-0">
+                <span className={"fw-bold"}>Status: </span>
+                {table.status}
+              </p>
+              <Button
+                variant="danger"
+                className="ms-auto me-2"
+                onClick={() => handleClick(table.id)}
+              >
+                Remove
+              </Button>
+              <Button variant="primary" as={NavLink} to={`/table/${table.id}`}>
+                Show more
+              </Button>
             </Col>
             <hr />
           </div>
-        ))
-      }
-    </Row>
-  );
+        ))}
+      </Row>
 
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Are you sure?</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          This operation will completely remove this table from the server.
+          <br />
+          Are you sure you want to do this?
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Cancel
+          </Button>
+          <Button variant="danger" onClick={() => handleRemove(tableOutId)}>
+            Remove
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
 };
 
 export default Tables;
